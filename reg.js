79f8feff61302b359001198b5b52a3991aa6a26c
dennis-lifepowr <110597328+dennis-lifepowr@users.noreply.github.com>
2024-09-26 14:08:05 +0200

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,7 +1,4 @@
-// Register AVIF polyfill directly using a CDN
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/npm/avif.js';
-script.onload = () => {
-    avif.register('/avif-sw.js'); // Make sure avif-sw.js is served correctly
-};
-document.body.appendChild(script);
+<script src="https://cdn.jsdelivr.net/npm/avif.js"></script>
+<script>
+  avif.register("/avif-sw.js");
+</script>
